53a556fecd4a5a2cc363fd7fb5a716ee84910a43
Carlos Garizado <cgarrizado1999@gmail.com>
2025-07-28 22:03:02 -0500

    add structure

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://x8ki-letl-twmt.n7.xano.io/api:UTdf2ovD'
+  };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'https://x8ki-letl-twmt.n7.xano.io/api:UTdf2ovD'
+  };
+  
